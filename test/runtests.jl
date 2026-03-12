8804ab3a76f7411c1999077d0b55bd187c3d3363
Junfeng Qiao <qiaojunfeng@outlook.com>
2026-03-12 12:13:18 +0100

    Allow running multiple tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,22 +2,23 @@ using TestItemRunner
 using CrystalBase
 using Documenter
 
-# Get filter string from command line arguments if provided
-# Usage: julia --project test/runtests.jl "Name of Test"
-filter_name = length(ARGS) > 0 ? ARGS[1] : nothing
+# Get filter strings from command line arguments if provided
+# Usage: julia --project test/runtests.jl "kpath.jl" "type.jl"
+filter_names = isempty(ARGS) ? nothing : ARGS
 
-if isnothing(filter_name)
+if isnothing(filter_names)
     println("Running all tests...")
+
+    @run_package_tests verbose = true
 
     DocMeta.setdocmeta!(CrystalBase, :DocTestSetup, :(using CrystalBase); recursive=true)
     doctest(
         CrystalBase,
         # fix=true,  # update all the output in `jldoctest`
     )
-
-    @run_package_tests verbose = true
 else
-    println("Running specific test: $filter_name")
+    println("Running specific tests: $(join(filter_names, ", "))")
 
-    @run_package_tests verbose = true filter = ti -> endswith(ti.filename, filter_name)
+    @run_package_tests verbose = true filter =
+        ti -> any(name -> endswith(ti.filename, name), filter_names)
 end
